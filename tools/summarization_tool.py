2b70e86e081234d1dff819206d2cc7882f3edaf0
Hrithik450 <mhrithik450@gmail.com>
2026-02-19 20:36:02 +0530

    updated gemeni api key in summarization tool

--- tools/summarization_tool.py
+++ tools/summarization_tool.py
@@ -9,7 +9,7 @@ from langchain_core.output_parsers import StrOutputParser
 import os
 
 # --- LLM and Prompt Initialization ---
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.1, google_api_key=os.getenv("GOOGLE_API_KEY"))
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.1, google_api_key=os.getenv("GEMINI_API_KEY"))
 encoding = tiktoken.get_encoding("cl100k_base")
 
 # Prompt for the "Map" step (summarizing a single chunk)
